864cadbc00781987ee11a835f21f57b0bca2d359
kidpixo <kidpixo@gmail.com>
2016-03-30 13:49:08 +0200

    added local file lister to bdm.py

--- bdm.py
+++ bdm.py
@@ -27,4 +27,22 @@ def remote_scraper(verbose=False):
 
     return chapter_dict
 
+def locale_scraper(verbose=False)
 
+    import os
+    import zipfile
+    import re
+
+    files = [f for f in  os.listdir('./comics/')  if re.match(r'.*.cbz$', f)]
+
+    chapter_dict = {}
+    for ch_filename in files:
+        with zipfile.ZipFile('comics/'+ch_filename, mode='r') as zf:
+            # getting only the files with path 'images/four digits'
+            chapter_dict[ch_filename] = [f for f in zf.namelist() if re.match(r'.*/\d{4}\..*', f)]
+        if verbose:
+            print 'chapter %s : ' % ch_filename
+            print chapter_dict[ch_filename]
+            print '-'*20
+
+        return chapter_dict
